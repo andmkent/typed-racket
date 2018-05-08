a909c701d4fe6a00d84422b305fc6954e9260ff2
Andrew Kent <pnwamk@gmail.com>
2018-05-08 08:45:11 -0400

    tweak kw/opt arg tests

--- typed-racket-test/fail/opt-bad-default-nonimmed.rkt
+++
@@ -1,5 +0,0 @@
-#lang typed/racket
-
-(: f (-> Integer Integer))
-(define (f x [y (string-append "x" "y")])
-  (+ x y))
--- typed-racket-test/fail/opt-kw-bad-default.rkt
+++
@@ -1,5 +0,0 @@
-#lang typed/racket
-
-(: f (-> Integer Integer))
-(define (f x #:y [y 'y])
-  (+ x y))
